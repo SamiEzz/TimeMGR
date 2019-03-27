8855013d6f58c2aa1c4fd3940815b620f7468024
Esami <sami.ezzerouali@hotmail.com>
2019-03-27 16:38:29 +0100

    compare to string(0) not 0

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def main_program():
     task1.start_act()
     task1.project="TIME_MGR"
     task1.comments="First task ever !"
-    while end_work==0:
+    while end_work!="1":
         print("Press '0' to stop counter")
         user_in = input()
-        if user_in==0 :
-            end_work=1
+        if user_in=="0" :
+            end_work="1"
             task1.end_act()
     task1.export2json()
 
